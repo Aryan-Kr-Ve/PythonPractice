6432fb6f59dcb29f7bafc83b43b87e447106182f
Aryan <142231398+Aryan-Kr-Ve@users.noreply.github.com>
2026-05-09 18:49:59 +0530

    MY First Day of Python Practice

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+#print("Aryan Verma")
+#print("Aryan Verma","\nRoll_No = 81")
+name="Aryan"
+age=25
+print(type(name),"\n",type(age))
+
+a=22
+b=45
+c=a+b
+print(c,"\n",type(c))
---
+++ PythonPracticeDay1.py
@@ -0,0 +1,8 @@
+# Type Casting
+a=int(input("Enter a 1st number:"))
+b=int(input("Enter a 2nd number:"))
+print("Sum of two numbers:",a+b)
+print("Average of Two Numbers:",(a+b)/2,type(a+b/2))
+
+print("Square root of a=",a**2,"\nSquare root of b=",b**2)
+print("Is a and b equal",a==b,type(a==b))
